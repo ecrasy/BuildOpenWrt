856e1dcc196678008601b80fa58d0ce861bd7bef
Carbon <ecrasy@gmail.com>
2022-10-01 19:06:16 +0800

    update sh files in data

--- data/model.sh
+++ data/model.sh
@@ -3,14 +3,14 @@
 # Author: Carbon (ecras_y@163.com)
 # Description: replace default model string with empty string
 # Created Time: 2022-07-25 01:30:41 UTC
-# Modified Time: 2022-07-25 01:31:41 UTC
+# Modified Time: 2022-10-01 11:02:32 UTC
 #########################################################################
 
 
 #!/bin/bash
 
-model="Default string Default string/Default string - "
-str=""
-command=s@$model@$str@
-sed -i "$command" /tmp/sysinfo/model
+str="Default string Default string/Default string - "
+rpl=""
+cmd=s@$str@$rpl@
+sed -i "$cmd" /tmp/sysinfo/model
 
