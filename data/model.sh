02e19d454abca6736d4d2aae8b3fb1331d29d0cb
Carbon <ecrasy@gmail.com>
2022-10-01 20:54:48 +0800

    update model sh

--- data/model.sh
+++ data/model.sh
@@ -1,9 +1,10 @@
 #########################################################################
-# File Name: data/model.sh
+# File Name: model.sh
 # Author: Carbon (ecras_y@163.com)
 # Description: replace default model string with empty string
+# will be called in wlan up event
 # Created Time: 2022-07-25 01:30:41 UTC
-# Modified Time: 2022-10-01 11:02:32 UTC
+# Modified Time: 2022-10-01 12:54:30 UTC
 #########################################################################
 
 
