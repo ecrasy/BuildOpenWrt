0bb72f6851c5201713a25a05206f3ba2d7c13350
Carbon <ecrasy@gmail.com>
2022-10-24 15:57:02 +0800

    update model.sh

--- data/model.sh
+++ data/model.sh
@@ -4,14 +4,19 @@
 # Description: replace default model string with empty string
 # will be called in wlan up event
 # Created Time: 2022-07-25 01:30:41 UTC
-# Modified Time: 2022-10-10 04:08:28 UTC
+# Modified Time: 2022-10-24 04:18:51 UTC
 #########################################################################
 
 
 #!/bin/bash
 
 str="Default string Default string/Default string - "
-rpl=""
+rpl="Intel(R) J4125"
+cmd="s@$str@$rpl@"
+sed -i "$cmd" /tmp/sysinfo/model
+
+str="Default string Default string"
+rpl="Intel(R) J4125"
 cmd="s@$str@$rpl@"
 sed -i "$cmd" /tmp/sysinfo/model
 
