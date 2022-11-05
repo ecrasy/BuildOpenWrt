36ada874beb64b7608a9fc70668d958c920cd9fd
Carbon <ecrasy@gmail.com>
2022-11-05 23:12:21 +0800

    update model.sh

--- data/etc/model.sh
+++ data/etc/model.sh
@@ -4,20 +4,28 @@
 # Description: replace default model string with empty string
 # will be called in wlan up event
 # Created Time: 2022-07-25 01:30:41 UTC
-# Modified Time: 2022-11-04 12:18:32 UTC
+# Modified Time: 2022-11-05 15:12:08 UTC
 #########################################################################
 
 
 #!/bin/sh
 
-str="Default string Default string/Default string - "
-rpl="Intel(R) J4125 - "
-cmd="s@$str@$rpl@"
+model=$(lscpu | grep "BIOS Model name" | sed -e "s|BIOS Model name:||g")
+if [ -z "$model" ]
+then
+    model="GenuineIntel"
+fi
+
+str="Default string Default string/Default string"
+rpl="$model"
+cmd="s|$str|$rpl|g"
+
 sed -i "$cmd" /tmp/sysinfo/model
 
+
 str="Default string Default string"
-rpl="Intel(R) J4125"
-cmd="s@$str@$rpl@"
+rpl="$model"
+cmd="s|$str|$rpl|g"
 sed -i "$cmd" /tmp/sysinfo/model
 
 # remove custom feeds form /etc/opkg/distfeeds.conf
