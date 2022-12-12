1702358f287677a2ee9b1a54e1a07c3eac373b99
Carbon <ecrasy@gmail.com>
2022-12-12 12:06:05 +0800

    update customize scripts

--- data/etc/model.sh
+++ data/etc/model.sh
@@ -4,7 +4,7 @@
 # Description: replace default model string with model name from lscpu
 # will be called in wlan up event
 # Created Time: 2022-07-25 01:30:41 UTC
-# Modified Time: 2022-11-10 01:34:36 UTC
+# Modified Time: 2022-12-12 04:05:41 UTC
 #########################################################################
 
 
@@ -27,6 +27,11 @@ cmd="s|$str|$rpl|g"
 sed -i "$cmd" /tmp/sysinfo/model
 
 str="Default string Default string"
+rpl="$model"
+cmd="s|$str|$rpl|g"
+sed -i "$cmd" /tmp/sysinfo/model
+
+str="Default string"
 rpl="$model"
 cmd="s|$str|$rpl|g"
 sed -i "$cmd" /tmp/sysinfo/model
