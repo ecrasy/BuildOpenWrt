e80bad5e0cda4976eff34b7992f85ac2c612e2ed
Carbon <ecrasy@gmail.com>
2022-12-17 22:36:50 +0800

    update configs:
    add back dnsmasq v2.87 nftset support
    
    update fix.sh

--- fix.sh
+++ fix.sh
@@ -3,28 +3,11 @@
 # Author: Carbon (ecrasy@gmail.com)
 # Description: feel free to use
 # Created Time: 2022-07-30 04:57:44 UTC
-# Modified Time: 2022-12-17 02:37:34 UTC
+# Modified Time: 2022-12-17 14:29:30 UTC
 #########################################################################
 
 
 #!/bin/bash
-
-# fix cjdns compile error
-# sed -i 's/ -Wno-error=stringop-overflow//g' package/feeds/routing/cjdns/Makefile
-# sed -i 's/ -Wno-error=stringop-overread//g' package/feeds/routing/cjdns/Makefile
-# echo "Fix cjdns makefile error!!!"
-
-# fix e2guardian compile error
-# sed -i 's/-fno-rtti/-fno-rtti -std=c++14/g' package/network/services/e2guardian/Makefile
-# echo "Fix e2guardian compile error!!!"
-
-# fix lua-eco recursive depend error
-# sed -i 's/ +PACKAGE_libwolfssl:libwolfssl//g' feeds/packages/lang/lua-eco/Makefile
-# echo "Fix lua-eco config error!!!"
-
-# fix qtbase package hash
-# sed -i '/official_releases/d' package/feeds/packages/qtbase/Makefile
-# echo "Fix qtbase hash error!!!"
 
 # fix error from https://github.com/openwrt/luci/issues/5373
 # luci-app-statistics: misconfiguration shipped pointing to non-existent directory
@@ -43,24 +26,32 @@ python3_path="feeds/packages/lang/python/python3"
 cp $GITHUB_WORKSPACE/data/patches/lib-platform-sys-version.patch ${python3_path}/patches/
 echo "Fix python host compile install error!!!"
 
-# fixing dnsmasq compile error
+# fixing dnsmasq v2.86 compile error
 # from: https://github.com/openwrt/openwrt/issues/9043
 dnsmasq_path="package/network/services/dnsmasq"
 dnsmasq_ver=$(grep 'PKG_UPSTREAM_VERSION:=2.86' ${dnsmasq_path}/Makefile)
 if [ ! -z "${dnsmasq_ver}" ]; then
     cp $GITHUB_WORKSPACE/data/patches/dnsmasq-struct-daemon.patch ${dnsmasq_path}/patches/
     echo "Fix dnsmasq v2.86 issue 9043"
+else
+# try nftables version 1.0.5 for dnsmasq v2.87
+    nftables_path="package/network/utils/nftables"
+    nftables_ver=$(grep 'PKG_VERSION:=0.9.6' ${nftables_path}/Makefile)
+    if [ ! -z "${nftables_ver}" ]; then
+        cp -rf $GITHUB_WORKSPACE/data/app/nftables/* ${nftables_path}/
+        echo "try nftables version 1.0.5 for dnsmasq v2.87"
+    fi
 fi
 
 # make minidlna depends on libffmpeg-full instead of libffmpeg
 # little bro ffmpeg mini custom be gone
 sed -i "s/libffmpeg /libffmpeg-full /g" feeds/packages/multimedia/minidlna/Makefile
-echo "Make minidlna depends on libffmpeg-full instead of libffmpeg"
+echo "Set minidlna depends on libffmpeg-full instead of libffmpeg"
 
 # make cshark depends on libustream-openssl instead of libustream-mbedtls
 # i fucking hate stupid mbedtls so much, be gone
 sed -i "s/libustream-mbedtls/libustream-openssl/g" feeds/packages/net/cshark/Makefile
-echo "Make cshark depends on libustream-openssl instead of libustream-mbedtls"
+echo "Set cshark depends on libustream-openssl instead of libustream-mbedtls"
 
 echo -e "FIX Completed!!!\n"
 
